53219a2215b8bbb314ea71b5d9194125cf48bf59
Dylan Barva <d.barva5402@gmail.com>
2019-06-26 18:02:31 -0700

    sending to kindle works, time for front end?

--- src/downloader.py
+++ src/downloader.py
@@ -1,14 +1,8 @@
 from fanfic_downloader import FanficDownloader
-
-# subprocess.call(["fanficfare", "-f", "mobi", "https://www.fanfiction.net/s/12133998/1/A-Patchwork-Knight"], cwd="C:/Users/dbarv/PycharmProjects/OmnibuserToKindle/build")
-
-# result = subprocess.check_output(["fanficfare", "-m", "https://www.fanfiction.net/s/12133998/1/A-Patchwork-Knight"])
-# print('687')
-# meta_data = result.decode('utf-8')
-# print(result.decode('utf-8'))
-# print(result[result.index('output_filename'): result.index('publisher')])
-# print(meta_data[meta_data.index('output_filename') + 19: meta_data.index('publisher') - 6])
+import password
+from email_sender import EmailSender
 
 fic = FanficDownloader("C:/Users/dbarv/PycharmProjects/StupidSummerProject/venv/Scripts/fanficfare.exe ", "https://www.fanfiction.net/s/12133998/1/A-Patchwork-Knight", "C:/Users/dbarv/PycharmProjects/StupidSummerProject/build")
-fic.download_story()
-print(fic.file_name)
+send = EmailSender(password.my_email, password.password, 587, password.kindle_email)
+send.send_fic(fic)
+
