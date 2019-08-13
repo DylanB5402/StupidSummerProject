f2ef57081b457433cb27c200e78c3bad7ef88ab7
Dylan Barva <d.barva5402@gmail.com>
2019-08-12 20:11:18 -0700

    add command line option

---
+++ src/fanfic_to_kindle.py
@@ -0,0 +1,16 @@
+from fanfic_downloader import FanficDownloader
+from email_sender import EmailSender
+import config
+import sys
+
+url = sys.argv[1]
+my_password = config.my_password
+email = config.my_email
+kindle_email = config.kindle_email
+fanficfare_path = config.fanficfare_path
+output_path = config.output_path
+
+fic = FanficDownloader(fanficfare_path,
+                       url, output_path)
+send = EmailSender(email, my_password, 587, kindle_email)
+send.send_fic(fic)
